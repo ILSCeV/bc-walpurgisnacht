020f9dbbc8ba07efb7f36d867950913e01a6af2a
Harfi <5414714-Harfi@users.noreply.gitlab.com>
2021-04-25 20:52:38 +0200

    Rumpelgasse eingefügt und Tiles eingefügt

---
+++ tiles/adaption.tsx
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="adaption" tilewidth="32" tileheight="32" tilecount="240" columns="16">
+ <image source="adaption.png" width="512" height="480"/>
+ <tile id="67">
+  <animation>
+   <frame tileid="67" duration="10000"/>
+   <frame tileid="99" duration="1000"/>
+   <frame tileid="67" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="68">
+  <animation>
+   <frame tileid="68" duration="1000"/>
+   <frame tileid="100" duration="1000"/>
+   <frame tileid="68" duration="10000"/>
+  </animation>
+ </tile>
+ <tile id="83">
+  <animation>
+   <frame tileid="83" duration="10000"/>
+   <frame tileid="115" duration="1000"/>
+   <frame tileid="83" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="84">
+  <animation>
+   <frame tileid="84" duration="1000"/>
+   <frame tileid="116" duration="1000"/>
+   <frame tileid="84" duration="10000"/>
+  </animation>
+ </tile>
+ <tile id="107">
+  <animation>
+   <frame tileid="106" duration="333"/>
+   <frame tileid="102" duration="333"/>
+   <frame tileid="103" duration="333"/>
+   <frame tileid="104" duration="333"/>
+   <frame tileid="105" duration="333"/>
+   <frame tileid="101" duration="9000"/>
+  </animation>
+ </tile>
+ <tile id="131">
+  <animation>
+   <frame tileid="131" duration="700"/>
+   <frame tileid="147" duration="700"/>
+  </animation>
+ </tile>
+ <tile id="132">
+  <animation>
+   <frame tileid="132" duration="7000"/>
+   <frame tileid="133" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="148">
+  <animation>
+   <frame tileid="148" duration="7000"/>
+   <frame tileid="149" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="164">
+  <animation>
+   <frame tileid="160" duration="1000"/>
+   <frame tileid="161" duration="1000"/>
+   <frame tileid="162" duration="1000"/>
+   <frame tileid="163" duration="1500"/>
+  </animation>
+ </tile>
+ <tile id="180">
+  <animation>
+   <frame tileid="176" duration="1000"/>
+   <frame tileid="177" duration="1000"/>
+   <frame tileid="178" duration="1000"/>
+   <frame tileid="179" duration="1500"/>
+  </animation>
+ </tile>
+ <tile id="196">
+  <animation>
+   <frame tileid="192" duration="1000"/>
+   <frame tileid="193" duration="1000"/>
+   <frame tileid="194" duration="1000"/>
+   <frame tileid="195" duration="1500"/>
+  </animation>
+ </tile>
+</tileset>
